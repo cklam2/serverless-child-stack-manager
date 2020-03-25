7e2586764c06122c73eee147a493cf3e66a8400f
coyoteecd <coyoteecd@gmail.com>
2020-03-25 17:26:12 +0200

    Publish files for v0.0.2

---
+++ dist/index.js
@@ -0,0 +1,107 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const serverless_stack_monitor_1 = require("./serverless-stack-monitor");
+class ServerlessStackSetManager {
+    constructor(serverless) {
+        this.serverless = serverless;
+        this.hooks = {
+            'before:remove:remove': async () => this.afterRemove()
+        };
+        this.provider = serverless.getProvider('aws');
+        this.stackMonitor = new serverless_stack_monitor_1.ServerlessStackMonitor(this.serverless, this.provider);
+    }
+    async afterRemove() {
+        const config = this.loadConfig();
+        if (config.removalPolicy === 'remove') {
+            this.serverless.cli.log(`Removing stacks prefixed with: ${config.childStacksNamePrefix}`);
+            const stackIds = await this.listMatchingStacks(config.childStacksNamePrefix);
+            this.serverless.cli.log(`Found ${stackIds.length} stacks`);
+            if (stackIds.length > 0) {
+                this.serverless.cli.log('Starting delete operation');
+                await this.deleteStacks(stackIds, config.maxConcurrentCount);
+            }
+            this.serverless.cli.log('Stacks removed successfully');
+        }
+        else {
+            this.serverless.cli.log('Skipping remove of child stacks because of removalPolicy setting');
+        }
+    }
+    async listMatchingStacks(childStacksNamePrefix) {
+        // Only look at the stacks which are in a "stable" state
+        const params = {
+            StackStatusFilter: [
+                'CREATE_COMPLETE',
+                'ROLLBACK_COMPLETE',
+                'UPDATE_COMPLETE',
+                'IMPORT_COMPLETE'
+            ]
+        };
+        const stackIds = [];
+        while (true) {
+            const result = await this.provider.request('CloudFormation', 'listStacks', params);
+            if (result.StackSummaries) {
+                const matchingStackIds = result.StackSummaries.filter(s => s.StackName.startsWith(childStacksNamePrefix))
+                    .filter(s => s.StackId)
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    .map(s => s.StackId);
+                stackIds.push(...matchingStackIds);
+            }
+            if (result.NextToken) {
+                params.NextToken = result.NextToken;
+            }
+            else {
+                // no more data, stop
+                break;
+            }
+        }
+        return stackIds;
+    }
+    async deleteStacks(stackIds, maxConcurrentCount) {
+        // We don't delete everything at once, because we risk being throttled by AWS
+        //
+        // This map tracks the stack IDs being deleted and the related promises.
+        // Its size is capped to maxParallelDeletes value;
+        // once an operation completes, we remove it from the map and add another
+        // so as to maintain the same number of ongoing delete operations.
+        const ongoingDeletes = new Map();
+        // mutable queue of stack IDs to delete
+        const queuedStackIds = Array.from(stackIds);
+        // set up the initial batch
+        queuedStackIds.splice(0, maxConcurrentCount).forEach(stackId => {
+            ongoingDeletes.set(stackId, this.deleteStack(stackId));
+        });
+        while (true) {
+            const deletedStackId = await Promise.race(ongoingDeletes.values());
+            this.serverless.cli.log(`Stack ${deletedStackId} successfully deleted`);
+            // remove the completed promise and queue another delete operation to keep CloudFormation busy
+            ongoingDeletes.delete(deletedStackId);
+            const nextStackId = queuedStackIds.pop();
+            if (nextStackId) {
+                ongoingDeletes.set(nextStackId, this.deleteStack(nextStackId));
+            }
+            // see if there's more work to do
+            if (ongoingDeletes.size === 0) {
+                break;
+            }
+        }
+    }
+    async deleteStack(stackId) {
+        const deleteParams = {
+            StackName: stackId
+        };
+        await this.provider.request('CloudFormation', 'deleteStack', deleteParams);
+        return this.stackMonitor.monitor('removal', stackId).then(() => stackId);
+    }
+    loadConfig() {
+        const providedConfig = this.serverless.service.custom['serverless-child-stack-manager'];
+        if (!providedConfig.childStacksNamePrefix) {
+            throw new Error('childStacksNamePrefix is required');
+        }
+        return {
+            childStacksNamePrefix: providedConfig.childStacksNamePrefix,
+            removalPolicy: providedConfig.removalPolicy || 'keep',
+            maxConcurrentCount: providedConfig.maxConcurrentCount || 5
+        };
+    }
+}
+module.exports = ServerlessStackSetManager;
